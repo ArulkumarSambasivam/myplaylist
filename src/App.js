1ed8c1e0c044aab76f94eb763d3b9d8ca2d506d6
ArulkumarSambasivam <arulkumarpmp@gmail.com>
2017-10-08 23:06:28 +0530

    routing

--- src/App.js
+++ src/App.js
@@ -22,13 +22,14 @@ class App extends Component {
 const BasicExample = () => (
   <Router>
     <div>
-      <ul>
+    <nav class="navbar navbar-default">
+      <ul class="nav navbar-nav">
         <li><Link to="/">Home</Link></li>
         <li><Link to="/about">About</Link></li>
         <li><Link to="/topics">Topics</Link></li>
       </ul>
-
-      <hr/>
+   </nav>
+   
 
       <Route exact path="/" component={Home}/>
       <Route path="/about" component={About}/>
